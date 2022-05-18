e54f71afb279dc04bed65e04243fc9c9d573b28d
Sinan Sonmez (Chaush) <37421564+sinansonmez@users.noreply.github.com>
2022-05-18 04:07:27 +0200

    experiment with shapes

--- src/taskpane/components/App.tsx
+++ src/taskpane/components/App.tsx
@@ -59,6 +59,42 @@ export default class App extends React.Component<AppProps, AppState> {
     );
   };
 
+  insertBox = async () => {
+    // This sample creates a rectangle positioned 100 points from the top and left sides
+    // of the slide and is 150x150 points. The shape is put on the first slide.
+    await PowerPoint.run(async (context) => {
+      await context.sync();
+      // @ts-ignore
+      const shapes = context.presentation.slides.getItemAt(0).shapes;
+      const rectangle = shapes.addGeometricShape(PowerPoint.GeometricShapeType.diamond);
+      rectangle.left = 100;
+      rectangle.top = 100;
+      rectangle.height = 150;
+      rectangle.width = 150;
+      rectangle.fill.color.rgb = "FF0000";
+      rectangle.name = "Square";
+      await context.sync();
+    });
+  };
+
+  deleteShapes = async () => {
+    await PowerPoint.run(async (context) => {
+      // Delete all shapes from the first slide.
+      const sheet = context.presentation.slides.getItemAt(0);
+      // @ts-ignore
+      const shapes = sheet.shapes;
+
+      // Load all the shapes in the collection without loading their properties.
+      shapes.load("items/$none");
+      await context.sync();
+
+      shapes.items.forEach(function (shape) {
+        shape.delete();
+      });
+      await context.sync();
+    });
+  };
+
   render() {
     const { title, isOfficeInitialized } = this.props;
 
@@ -74,15 +110,15 @@ export default class App extends React.Component<AppProps, AppState> {
 
     return (
       <div className="ms-welcome">
-        <Header logo={require("./../../../assets/logo-filled.png")} title={this.props.title} message="Welcome" />
-        <HeroList message="Discover what Office Add-ins can do for you today!" items={this.state.listItems}>
-          <p className="ms-font-l">
-            Modify the source files, then click <b>Run</b>.
-          </p>
           <DefaultButton className="ms-welcome__action" iconProps={{ iconName: "ChevronRight" }} onClick={this.click}>
             Run
           </DefaultButton>
-        </HeroList>
+          <DefaultButton className="ms-welcome__action" iconProps={{ iconName: "ChevronLeft" }} onClick={this.insertBox}>
+            Add a box
+          </DefaultButton>
+          <DefaultButton className="ms-welcome__action" iconProps={{ iconName: "ChevronLeft" }} onClick={this.deleteShapes}>
+            Delete all shapes
+          </DefaultButton>
       </div>
     );
   }
